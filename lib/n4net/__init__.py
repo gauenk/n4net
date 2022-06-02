440f7aa6557d856c04ce77ea7265ef84f717e228
gauenk <kent.gauen@gmail.com>
2022-06-02 17:17:35 -0400

    batched test runs

--- lib/n4net/__init__.py
+++ lib/n4net/__init__.py
@@ -1,5 +1,7 @@
 from . import model
 from .model import N4Net,ArchitectureOptions,load_model
+from . import lidia
+from . import batched_lidia
 
 
 
--- tests/test_same_lidia.py
+++ tests/test_same_lidia.py
@@ -29,6 +29,7 @@ import data_hub
 
 # -- package imports [to test] --
 import n4net
+import dnls # supporting
 
 # -- package imports [to test] --
 import lidia
@@ -40,7 +41,7 @@ from lidia.data import save_burst
 from scipy import optimize
 MAX_NFRAMES = 85
 DATA_DIR = Path("./data/")
-SAVE_DIR = Path("./output/tests/")
+SAVE_DIR = Path("./output/tests/test_same_lidia/")
 if not SAVE_DIR.exists():
     SAVE_DIR.mkdir(parents=True)
 
@@ -73,7 +74,7 @@ class TestSameLidia(unittest.TestCase):
         burst = th.from_numpy(burst).type(th.float32)
         return burst
 
-    def test_same_lidia(self):
+    def skip_test_same_lidia(self):
 
         # -- params --
         sigma = 50.
@@ -101,17 +102,68 @@ class TestSameLidia(unittest.TestCase):
 
             # -- lidia exec --
             lidia_model = get_lidia_model_nl(device,im_shape,sigma)
-            deno_steps = lidia_model.run_parts(noisy.clone(),sigma,train=train)
+            deno_steps = lidia_model.run_parts(noisy,sigma,train=train)
             deno_steps = deno_steps.detach()
 
             # -- n4net exec --
-            n4_model = n4net.load_model(sigma)
-            deno_n4 = n4_model(noisy.clone(),sigma,train=train)
+            n4_model = n4net.lidia.load_model(sigma)
+            deno_n4 = n4_model(noisy,sigma,train=train)
             deno_n4 = deno_n4.detach()
 
             # -- test --
-            print("deno_n4.shape: ",deno_n4.shape)
-            print("deno_steps.shape: ",deno_steps.shape)
             error = th.sum((deno_n4 - deno_steps)**2).item()
             error < 1e-10
+
+    def test_batched(self):
+
+        # -- params --
+        sigma = 50.
+        device = "cuda:0"
+        ps = 5
+        vid_set = "toy"
+        vid_name = "text_tourbus"
+
+        # -- video --
+        vid_cfg = data_hub.get_video_cfg(vid_set,vid_name)
+        clean = data_hub.load_video(vid_cfg)[:3,:,:96,:128]
+        clean = th.from_numpy(clean).contiguous().to(device)
+
+        # -- set seed --
+        seed = 123
+        th.manual_seed(seed)
+        np.random.seed(seed)
+
+        # -- over training --
+        for train in [True]:#,False]:
+
+            # -- get data --
+            noisy = clean + sigma * th.randn_like(clean)
+            im_shape = noisy.shape
+            noisy = noisy.contiguous()
+
+            # -- lidia exec --
+            # lidia_model = get_lidia_model_nl(device,im_shape,sigma)
+            # deno_steps = lidia_model.run_parts(noisy,sigma,train=train)
+            # deno_steps = deno_steps.detach()
+
+            # -- n4net exec --
+            n4_model = n4net.lidia.load_model(sigma)
+            deno_steps = n4_model(noisy,sigma,train=train)
+            deno_steps = deno_steps.detach()
+
+            # -- n4net exec --
+            n4b_model = n4net.batched_lidia.load_model(sigma)
+            deno_n4 = n4b_model(noisy,sigma,train=train)
+            deno_n4 = deno_n4.detach()
+
+            # -- save --
+            deno_n4 /= deno_n4.max()
+            deno_steps /= deno_steps.max()
+            dnls.testing.data.save_burst(deno_n4,SAVE_DIR,"batched")
+            dnls.testing.data.save_burst(deno_steps,SAVE_DIR,"ref")
+
+            # -- test --
+            error = th.sum((deno_n4 - deno_steps)**2).item()
+            error < 1e-10
+
 
